a9f2fb6e9f60ea7cf2c058553c76d9c9ed23985a
CapFreddy <fred9918@hotmail.com>
2022-01-07 19:39:32 +0800

    First commit

---
+++ raw/download.sh
@@ -0,0 +1,10 @@
+wget https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/bace.csv
+wget https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/BBBP.csv
+wget https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/clintox.csv.gz
+wget https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/sider.csv.gz
+wget https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/tox21.csv.gz
+wget https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/toxcast_data.csv.gz
+wget https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/HIV.csv
+wget https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/muv.csv.gz
+
+gunzip *.gz
---
+++ splitter.py
@@ -0,0 +1,149 @@
+import numpy as np
+import pandas as pd
+from rdkit import Chem
+from rdkit.Chem.Scaffolds import MurckoScaffold
+from sklearn.model_selection import train_test_split, KFold
+
+import os
+import os.path as osp
+from collections import defaultdict
+
+from typing import Tuple, Iterable, Optional
+
+from .utils import dataset_utils
+
+
+#################### Random split: K-fold cross validation ####################
+"""Generate indices or dataset slices in randomized K-fold splitting"""
+def k_fold_cross_validation(dataset: str,
+                            data_list: Optional[Iterable] = None,
+                            n_splits: int = 10,
+                            random_state: int = 0) -> Tuple[Iterable, Iterable, Iterable]:
+    assert dataset in dataset_utils.random_split, f'{dataset} is not suitable for random split.'
+
+    df = filtered_dataset(dataset)
+    if data_list is not None:
+        assert len(data_list) == len(df), 'Length of `data_list` does not match filtered dataset.'
+
+    skf = KFold(n_splits=n_splits, shuffle=True, random_state=random_state)
+    for train_idx, test_idx in skf.split(df):
+        train_idx, val_idx = train_test_split(train_idx, test_size=1/9, random_state=random_state)
+        if data_list is not None:
+            yield data_list[train_idx], data_list[val_idx], data_list[test_idx]
+        else:
+            yield train_idx, val_idx, test_idx
+
+
+#################### Scaffold split: Deterministic & randomized ####################
+"""Deterministic scaffold split"""
+def scaffold_split(dataset: str, data_list: Optional[Iterable] = None) \
+                   -> Tuple[Iterable, Iterable, Iterable]:
+    assert dataset in dataset_utils.scaffold_split, f'{dataset} is not suitable for scaffold split.'
+
+    df = filtered_dataset(dataset)
+    if data_list is not None:
+        assert len(data_list) == len(df), 'Length of `data_list` does not match filtered dataset.'
+
+    # Create dict of the form { scaffold_i: [idx_1, idx...] }
+    all_scaffold = defaultdict(list)
+    smiles_sr = df.loc[:, dataset_utils.smiles_col[dataset]]
+    for idx, smiles in enumerate(smiles_sr):
+        scaffold = MurckoScaffold.MurckoScaffoldSmiles(smiles, includeChirality=True)
+        all_scaffold[scaffold].append(idx)
+
+    # Sort from largest to smallest sets
+    all_scaffold = { key: sorted(value) for key, value in all_scaffold.items() }
+    all_scaffold_set = [
+        scaffold_set for (scaffold, scaffold_set) in sorted(
+            all_scaffold.items(), key=lambda x: (len(x[1]), x[1][0]), reverse=True
+        )
+    ]
+
+    # Get train, val, test indices
+    train_idx, val_idx, test_idx = [], [], []
+    train_cutoff = len(df) * 0.8
+    val_cutoff = len(df) * (0.8 + 0.1)
+
+    for scaffold_set in all_scaffold_set:
+        if len(train_idx) + len(scaffold_set) > train_cutoff:
+            if len(train_idx) + len(val_idx) + len(scaffold_set) > val_cutoff:
+                test_idx.extend(scaffold_set)
+            else:
+                val_idx.extend(scaffold_set)
+        else:
+            train_idx.extend(scaffold_set)
+
+    if data_list is not None:
+        return data_list[train_idx], data_list[val_idx], data_list[test_idx]
+
+    return train_idx, val_idx, test_idx
+
+
+"""Randomized scaffold split"""
+def random_scaffold_split(dataset: str, data_list: Optional[Iterable] = None, random_state: int = 0) \
+                          -> Tuple[Iterable, Iterable, Iterable]:
+    assert dataset in dataset_utils.scaffold_split, f'{dataset} is not suitable for scaffold split.'
+
+    df = filtered_dataset(dataset)
+    if data_list is not None:
+        assert len(data_list) == len(df), 'Length of `data_list` does not match filtered dataset.'
+
+    # Create dict of the form { scaffold_i: [idx_1, idx...] }
+    all_scaffold = defaultdict(list)
+    smiles_sr = df.loc[:, dataset_utils.smiles_col[dataset]]
+    for idx, smiles in enumerate(smiles_sr):
+        scaffold = MurckoScaffold.MurckoScaffoldSmiles(smiles, includeChirality=True)
+        all_scaffold[scaffold].append(idx)
+
+    # Randomly permutate scaffold sets
+    all_scaffold_set = list(all_scaffold.values())
+    rng = np.random.RandomState(random_state)
+    perm = rng.permutation(len(all_scaffold_set))
+    all_scaffold_set = [all_scaffold_set[perm_idx] for perm_idx in perm]
+
+    # Get train, val, test indices
+    train_idx, val_idx, test_idx = [], [], []
+    train_cutoff = len(df) * 0.8
+    val_cutoff = len(df) * (0.8 + 0.1)
+
+    for scaffold_set in all_scaffold_set:
+        if len(train_idx) + len(scaffold_set) > train_cutoff:
+            if len(train_idx) + len(val_idx) + len(scaffold_set) > val_cutoff:
+                test_idx.extend(scaffold_set)
+            else:
+                val_idx.extend(scaffold_set)
+        else:
+            train_idx.extend(scaffold_set)
+
+    if data_list is not None:
+        return data_list[train_idx], data_list[val_idx], data_list[test_idx]
+
+    return train_idx, val_idx, test_idx
+
+
+#################### Filter unparsable SMILES ####################
+ABS_DIR = osp.dirname(osp.abspath(__file__))
+
+
+"""Obtain filtered dataset and cache"""
+def filtered_dataset(dataset: str) -> pd.DataFrame:
+    filtered_dir = osp.join(ABS_DIR, 'filtered')
+    if not osp.exists(filtered_dir):
+        os.mkdir(filtered_dir)
+
+    filtered_path = osp.join(filtered_dir, f'{dataset}.csv')
+    if not osp.exists(filtered_path):
+        raw_path = osp.join(ABS_DIR, 'raw', dataset_utils.raw_file[dataset])
+        smiles_col = dataset_utils.smiles_col[dataset]
+        filtered_df = filter_unparsable(pd.read_csv(raw_path), smiles_col)
+        filtered_df.to_csv(filtered_path, index=False)
+
+    return pd.read_csv(filtered_path)
+
+
+"""Filter unparsable SMILES"""
+def filter_unparsable(df: pd.DataFrame, smiles_col: str) -> pd.DataFrame:
+    smiles_sr = df.loc[:, smiles_col]
+    mol_sr = smiles_sr.apply(Chem.MolFromSmiles)
+    filtered_df = df.loc[~mol_sr.isna()].reset_index(drop=True)
+    return filtered_df
